545682688ba872301c62713e5ebb87edc27ed03e
Eugene <vasyl.palianytsia.dos@gmail.com>
2022-02-28 22:12:41 +0200

    Allowed few headers

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ fs.readFile('index.html').then((content) => {
   index = content.toString();
 });
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/', (req, res) => {
   res.send(index);
 });
